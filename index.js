cdeeef10e46281288d2968ec13d2e4df8118848b
AeahKa <aeah@qq.com>
2023-05-22 18:30:25 +0800

    优化代码

--- index.js
+++ index.js
@@ -1,6 +1,71 @@
 const db = require('./db')
 const inquirer = require('@inquirer/prompts')
-
+function addTask(list) {
+	inquirer.input({ message: 'Enter a task' }).then((answer3) => {
+		list.push({
+			title: answer3,
+			done: false,
+		})
+		db.write(list)
+		console.log('succeed')
+	})
+}
+function askForAction(list, index) {
+	inquirer
+		.select({
+			message: 'selected',
+			choices: [
+				{ name: 'Quit', value: 'quit' },
+				{ name: 'Done', value: 'done' },
+				{ name: 'Undone', valUe: 'undone' },
+				{ name: 'Remove', value: 'remove' },
+			],
+		})
+		.then((answer2) => {
+			switch (answer2) {
+				case 'quit':
+					break
+				case 'done':
+					list[index].done = true
+					db.write(list)
+					break
+				case 'undone':
+					list[index].done = false
+					db.write(list)
+					break
+				case 'remove':
+					list.splice(index, 1)
+					db.write(list)
+					break
+			}
+		})
+}
+function printAllTasks(list) {
+	inquirer
+		.select({
+			message: 'selected',
+			choices: [
+				{ name: 'Quit', value: '-2' },
+				{ name: 'Add a new task', value: '-1' },
+				...list.map((task, index) => {
+					return {
+						name: `${task.done ? '[Done]' : '[____]'} ${index + 1} - ${
+							task.title
+						}`,
+						value: index.toString(),
+					}
+				}),
+			],
+		})
+		.then((answer) => {
+			const index = parseInt(answer)
+			if (index >= 0) {
+				askForAction(list, index)
+			} else if (index === -1) {
+				addTask(list)
+			}
+		})
+}
 module.exports = {
 	add: async (title) => {
 		// 读取之前的任务
@@ -17,62 +82,6 @@ module.exports = {
 
 	showAll: async () => {
 		const list = await db.read()
-		inquirer
-			.select({
-				message: 'selected',
-				choices: [
-					{ name: 'Quit', value: '-2' },
-					{ name: 'Add a new task', value: '-1' },
-					...list.map((task, index) => {
-						return {
-							name: `${task.done ? '[Done]' : '[____]'} ${index + 1} - ${
-								task.title
-							}`,
-							value: index.toString(),
-						}
-					}),
-				],
-			})
-			.then((answer) => {
-				const index = parseInt(answer)
-				if (index >= 0) {
-					inquirer
-						.select({
-							message: 'selected',
-							choices: [
-								{ name: 'Quit', value: 'quit' },
-								{ name: 'Done', value: 'done' },
-								{ name: 'Undone', valUe: 'undone' },
-								{ name: 'Remove', value: 'remove' },
-							],
-						})
-						.then((answer2) => {
-							switch (answer2) {
-								case 'quit':
-									break
-								case 'done':
-									list[index].done = true
-									db.write(list)
-									break
-								case 'undone':
-									list[index].done = false
-									db.write(list)
-									break
-								case 'remove':
-									list.splice(index, 1)
-									db.write(list)
-									break
-							}
-						})
-				} else if (index === -1) {
-					inquirer.input({ message: 'Enter a task' }).then((answer3) => {
-						list.push({
-							title: answer3,
-							done: false,
-						})
-						db.write(list)
-					})
-				}
-			})
+		printAllTasks(list)
 	},
 }
